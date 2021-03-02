9b34199f06cbb93d4ce1b6106e542d29a2f89d36
Shuwei Ren <w0444586@campus.nscc.ca>
2021-03-02 00:28:47 -0400

    Added code to remove previous layer

--- flights/main.js
+++ flights/main.js
@@ -7,85 +7,65 @@
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-    // L.marker([42, -60]).addTo(map)
-    //     .bindPopup('This is a sample popup. You can put any html structure in this including extra flight data. You can also swap this icon out for a custom icon. Some png files have been provided for you to use if you wish.')
-    //     .openPopup();
-
-    
-    // var plane = L.icon({
-    //     iconUrl:'plane.svg',
-    //     iconSize:[38,95]
-    // })
-    
-    
-      
-      // create the GeoJSON layer
-    // 
-         function fetchData(){
-            var data
-           fetch(`https://opensky-network.org/api/states/all`)
+    function fetchData(previousLayer=null){
+        fetch(`https://opensky-network.org/api/states/all`)
         .then(function(response){
             return response.json()
         })
         .then(function(data){
-           let newData = data.states.filter((state)=> state[2]=== "Canada").map(state=>{
-              return {
-                "type": "Feature",
-                "geometry": {
-                  "type": "Point",
-                  "coordinates": [state[5], state[6]]
-                },
-                "properties": {
-                    "country":state[2],
-                    "true_track": state[10],
-                    "position":[state[5],state[6]]
-                    
+        let newData = data.states.filter((state)=> state[2]=== "Canada").map(
+            state => {
+                return {
+                            "type": "Feature",
+                            "geometry": {
+                                    "type": "Point",
+                                    "coordinates": [state[5], state[6]]
+                                },
+                            "properties": {
+                                "country":state[2],
+                                "true_track": state[10],
+                                "position":[state[5],state[6]],
+                                "velocity": state[9],
+                                "vertical_rate": state[11]
+                            }
+                        }
+                }
+            )
 
-
-                } 
-            }
-         })
-
-         function createCustomIcon (feature, latlng) {
+        function createCustomIcon (feature, latlng) {
             let myIcon = L.icon({
                 iconUrl: 'plane3.png',
                 iconSize:     [25, 25], 
-                popupAnchor:  [0, 0] 
+                popupAnchor:  [0, 0]
             })
             return L.marker(latlng, { icon: myIcon, rotationAngle:feature.properties.true_track})
-          }
+        }
           
-          // create an options object that specifies which function will called on each feature
-          let myLayerOptions = {
-            pointToLayer: createCustomIcon
-          }
+        // create an options object that specifies which function will called on each feature
+        let myLayerOptions = {
+        pointToLayer: createCustomIcon
+        }
+        if (previousLayer !== null) {
+            previousLayer.clearLayers()
+        }
     
-          var myLayer=L.geoJSON(null,myLayerOptions)
-        console.log(newData) 
-        myLayer.clearLayers()
-        console.log(myLayer)
-        myLayer.addData(newData).addTo(map).bindPopup(function(layer){
-            return `I am origin from ${layer.feature.properties.country},</br> And my position is ${layer.feature.properties.position}`
-        })
+        var myLayer=L.geoJSON(null,myLayerOptions)
+        console.debug(newData)
+        
        
-        //  myLayer.addData(newData,myLayerOptions).addTo(map)
-        setTimeout(fetchData,4000)
-
+        myLayer.addData(newData).addTo(map).bindPopup(function(layer){
+            return `<p><label>originCountry:</label><span>${layer.feature.properties.country}</span></p>
+            <p><label>coordinates:</label><span>${layer.feature.properties.position}</span></p>
+            <p><label>velocity:</label><span>${layer.feature.properties.velocity}</span></p>
+            <p><label>vertical rate:</label><span>${layer.feature.properties.vertical_rate}</span></p>
+            `
+        })
         
-        
+        setTimeout(fetchData(myLayer),10000)
         })
     }
-    setTimeout(fetchData,4000)
+    setTimeout(fetchData,1000)
    
-    
-    
-   
-    
-    
-
-    
-
-
 })()
 
 
